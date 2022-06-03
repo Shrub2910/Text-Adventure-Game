0c01d7acd63eb25b1db9f24f443967151f500136
Shrub2910 <102828786+Shrub2910@users.noreply.github.com>
2022-06-03 18:32:30 +0100

    Add files via upload

--- Login.py
+++ Login.py
@@ -1,9 +1,10 @@
 from Main import main
-    
+import hashlib as hl   
 
 def register():
     username = input("Enter A Username")
     password = input("Enter A Password")
+    password = hl.sha256(password.encode('utf')).hexdigest()
     with open("login_info.txt", "a") as f:
         f.write(f"{username},{password}\n")
 
@@ -14,11 +15,14 @@ def login():
         for line in lines:
           if line.startswith(username):
             password = input("Enter A Password: ")
+            password = hl.sha256(password.encode('utf')).hexdigest()
             if line == username+","+password+"\n":
                 menu(True, username)
             else:
-                print("ERROR")
+                print("Password Incorrect")
                 menu(False, 0)
+    print("Couldn't Find Username")
+    menu(False, 0)
             
         
 
@@ -32,7 +36,7 @@ def menu(logged_in, username):
         option = input(">>> ")
         if option == "1":
           if logged_in == True:
-            main(username)
+            start()
           else:
             print("You Need To Login. If You Don't Have An Account Then Register")
         elif option == "2":
--- Main.py
+++ Main.py
@@ -1,20 +1,39 @@
 from time import sleep
-import sys
-import os
 game_over = False
-
+import os
 class Thing:
-
-  
   def __init__(self, name, description):
     self.name = name
     self.description = description
-
-
-class Room(Thing):
-
   
-  def __init__(self, name, description, n, s, w, e):
+class Player:
+  def __init__(self, room):
+    self.room = room
+
+  def move(self, Rooms, direction):
+    if direction == "n" and self.room.n != -1:
+      self.room = Rooms[self.room.n]
+    elif direction == "s" and self.room.s != -1:
+      self.room = Rooms[self.room.s]
+    elif direction == "w" and self.room.w != -1:
+      self.room = Rooms[self.room.w]
+    elif direction == "e" and self.room.e != -1:
+      self.room = Rooms[self.room.e]
+    else:
+      print("You Can't Go There")
+
+  def doors(self):
+    if self.room.n != -1:
+      print("North")
+    if self.room.s != -1:
+      print("South")
+    if self.room.w != -1:
+      print("West")
+    if self.room.e != -1:
+      print("East")
+    
+class Room(Thing):
+  def __init__(self, name, description,n, s, w, e):
     super().__init__(name, description)
     self.n = n
     self.s = s
@@ -22,108 +41,37 @@ class Room(Thing):
     self.e = e
 
 
-class Player(Thing):
-
-  
-  def __init__(self, name, description, room):
-    super().__init__(name, description)
-    self.room = room
-    
-
-  def move(self, rooms, direction):
-    if direction == "n" and self.room.n != -1:
-      self.room = rooms[self.room.n]
-    elif direction == "s" and self.room.s != -1:
-      self.room = rooms[self.room.s]
-    elif direction == "w" and self.room.w != -1:
-      self.room = rooms[self.room.w]
-    elif direction == "e" and self.room.e != -1:
-      self.room = rooms[self.room.e]
-    else:
-      typing_effect("You Can't Go There")
-
-
-  def doors(self, rooms):
-    if self.room.n != -1:
-      typing_effect("North")
-    if self.room.s != -1:
-      typing_effect("South")
-    if self.room.w != -1:
-      typing_effect("West")
-    if self.room.e != -1:
-      typing_effect("East")
-      
-
-def typing_effect(text):
-    for char in text:
-        sleep(0.05)
-        sys.stdout.write(char)
-        sys.stdout.flush()
-    sys.stdout.write("\n")
-    sys.stdout.flush()
-      
-def main(name):
-  os.system("cls")
-  
+def main():
   #Game Initialisation
-  command = ""
-  rooms = [
-    Room("Outside", "It is very dark", 1, -1, -1, -1),
-    Room("Reception", "For a reception it isn't very welcoming.", -1, 0, -1, 2),
-    Room("Hallway", "It's a very long passage.", 3, -1, 1, -1),
-    Room("Conference Room","The room is rusty, crusty and dusty",-1, 2, -1, -1)
+  Rooms = [
+    Room("Outside", "Very Dark", 1, -1, -1, 2),
+    Room("Entrance", "There is a dimly lit light bulb. Nothing else.", 3, 0, 4, -1),
+    Room("Green House", "Its very green", -1, -1, 0, -1),
+    Room("Hall", "The hall is extremely long", 5, 1, -1, -1),
+    Room("Basement", "It's extremely dark, you need to turn on a light", -1, -1, -1, 1),
+    Room("Living Room", "It's super cozy in here suprisingly", -1, 3, -1, -1)
   ]
-  player = Player(name, "player", rooms[0])
-
-  typing_effect(
-  """
-  COMMANDS:
-  • Help
-  • Where
-  • N/S/W/E 
-  """
-  )
-  
+  player = Player(Rooms[0])
   #Main Game Loop
-  while command != "q" and not game_over:
-      command = input(">>> ").strip().lower()
-      os.system("cls")
-      if command == "where":
-        typing_effect(f"Your location is {player.room.name}")
-        typing_effect(player.room.description)
-        typing_effect("Places You Can Go:")
-        player.doors(rooms)
-      elif command == "help":
-          typing_effect(
-        """
-        COMMANDS:
-        • Help
-        • Where
-        • N/S/W/E 
-        """
-        )
-      elif command == "n":
-        player.move(rooms, "n")
-        typing_effect("You Moved North")
-      elif command == "s":
-        player.move(rooms, "s")
-        typing_effect("You Moved South")
-      elif command == "w":
-        player.move(rooms, "w")
-        typing_effect("You Moved West")
-      elif command == "e":
-        player.move(rooms, "e")
-        typing_effect("You Moved East")
-      elif command == "q":
-        typing_effect("You Have Quit. Saving...")
-      else:
-        typing_effect("That isn't a command.")
-
-        
+  while not game_over:
+    command = input(">>> ").strip().lower()
+    os.system("cls")
+    if command == "where":
+      print(player.room.name)
+      print(player.room.description)
+      print("Places You Can Go:")
+      player.doors()
       
-    
+    elif command == "n":
+      player.move(Rooms, "n")
+    elif command == "s":
+      player.move(Rooms, "s")
+    elif command == "w":
+      player.move(Rooms, "w")
+    elif command == "e":
+      player.move(Rooms, "e")
+    else:
+      print("ERROR")
 
-
-
-
-
+main()
+  
